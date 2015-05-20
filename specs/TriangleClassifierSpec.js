8c31f45eec981db5a0546e05cb38f01ef935ca12
Steve Shreve <steveshreve@me.com>
2015-05-20 16:38:25 -0400

    new hanges

--- specs/TriangleClassifierSpec.js
+++ specs/TriangleClassifierSpec.js
@@ -17,21 +17,6 @@ describe('TriangleClassifier', function() {
     expect(TriangleClassifier).toBeDefined();
   });
 
-  it('classifies as scalene 4,3,2', function() {
-    var result = new TriangleClassifier().classify(4,3,2);
-    expect(result).toEqual('scalene');
-  });
-
-    it('classifies as equilateral 4,4,4', function() {
-    var result = new TriangleClassifier().classify(4,4,4);
-    expect(result).toEqual('equilateral');
-  });
-
-it('classifies as isosceles 4,4,2', function() {
-    var result = new TriangleClassifier().classify(4,4,2);
-    expect(result).toEqual('isosceles');
-  });
-
   // What happens when a side is 0?
   // What about a negative side?
 });
